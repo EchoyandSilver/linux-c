0238a977eab036e7887be2a2cd1842b70d6a78a3
贤 于 <EchoyandSliver>
2016-01-23 21:48:20 +0800

    add somefunction about socket

--- src/linux-c/07socket/echosrv.c
+++ src/linux-c/07socket/echosrv.c
@@ -11,47 +11,47 @@
 
 #define ERR_EXIT(m) \
         do \
-        {       \
-                perror(m);\
-                exit(EXIT_FAILURE);\
-        }while(0)
+        { \
+                perror(m); \
+                exit(EXIT_FAILURE); \
+        } while(0)
 
 int main(void)
 {
 	int listenfd;
-	if((listenfd = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0)
-/*	if((listend = socket(PF_INET,SOCK_STREAM,0))<0 )	*/
+	if ((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+/*	if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)*/
 		ERR_EXIT("socket");
-	
+
 	struct sockaddr_in servaddr;
-	memset(&servaddr,0,sizeof(servaddr));
+	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(5188);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	/*servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");*/
-	/*inet_aton("127.0.0.1",&servaddr.sin_addrd);*/
+	/*inet_aton("127.0.0.1", &servaddr.sin_addr);*/
 
-	if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
 		ERR_EXIT("bind");
-
-	if(listen(listenfd,SOMAXCONN) < 0)
+	if (listen(listenfd, SOMAXCONN) < 0)
 		ERR_EXIT("listen");
-	
+
 	struct sockaddr_in peeraddr;
 	socklen_t peerlen = sizeof(peeraddr);
 	int conn;
-	if((conn = accept(listenfd,(struct sockaddr*)&peeraddr,&peerlen)) < 0)
+	if ((conn = accept(listenfd, (struct sockaddr*)&peeraddr, &peerlen)) < 0)
 		ERR_EXIT("accept");
+
 	char recvbuf[1024];
-	while(1)
+	while (1)
 	{
-		memset(recvbuf,0,sizeof(recvbuf));	
-		int ret = read(listenfd,recvbuf,sizeof(recvbuf));
-		fputs(recvbuf,stdout);
-		write(conn,recvbuf,ret);
+		memset(recvbuf, 0, sizeof(recvbuf));
+		int ret = read(conn, recvbuf, sizeof(recvbuf));
+		fputs(recvbuf, stdout);
+		write(conn, recvbuf, ret);
 	}
-	close(listenfd);
 	close(conn);
+	close(listenfd);
+	
 	return 0;
 }
-
