814cc465471044666f4fa5023051b965ded80f71
贤 于 <EchoyandSliver>
2016-02-18 21:28:34 +0800

    add somefunction about C++

---
+++ src/linux-c/18cpp/03.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Singleton
+{
+public:
+	static Singleton& GetInstance()
+	{
+		static Singleton instance;
+		return instacne;
+	}
+	~Singleton()					//
+	{
+		cout<<"~Singleton..."<<endl;
+	}
+
+private:
+	Singleton(const Singleton& other);
+	Singleton& operator=(const Singleton& other);
+	Singleton()
+	{
+		cout<<"Singleton..."<<endl;
+	}
+
+};
+
+int main(void)
+{
+	Singleton& s1 = Singleton::GetInstance();
+	Singleton& s2 = Singleton::GetInstance();
+
+	return 0;
+}
