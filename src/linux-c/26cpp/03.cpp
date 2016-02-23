bad81ca1ac16a8302d60095859e47b6e95b68484
贤 于 <EchoyandSliver>
2016-02-23 18:40:54 +0800

    add somecpp about C++

---
+++ src/linux-c/26cpp/03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	string s1("abcdefghijkl");
+	s1.replace(2, 2, "AAAAA");
+	cout<<s1<<endl;
+	
+	s1 = "abcdefg";
+	s1.replace(s1.begin()+1, s1.begin()+4, "AAAAA");
+	cout<<s1<<endl;
+
+	string s2 = "xyzabc";
+	s2.insert(2, "MMMM");
+	cout<<s2<<endl;
+	s2.append("6666");
+	cout<<s2<<endl;
+
+	string s3="111";
+	s2.swap(s3);
+
+	cout<<s2<<endl;
+	cout<<s3<<endl;
+	return 0;
+}
---
+++ src/linux-c/26cpp/05.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string strinfo=" //*---Hello World!......------";
+	string strset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	string::size_type first = strinfo.find_first_of(strset);
+	if(first == string::npos)
+		cout<<"not find any charactors"<<endl;
+	string::size_type last = strinfo.find_last_of(strset);
+	if(last == string::npos)
+		cout<<"not find any charactors"<<endl;
+
+	cout<<strinfo.substr(first, last - first + 1)<<endl;
+
+	return 0;
+}
