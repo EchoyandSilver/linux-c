a6f6e135d5c048fc6b22deca0405ccd961b64068
贤 于 <EchoyandSliver>
2016-03-22 22:54:31 +0800

    add 36cpp and 36C++-note

---
+++ src/linux-c/36cpp/Shape.cpp
@@ -0,0 +1,32 @@
+#include "Shape.h"
+#include "DynBase.h"
+#include <iostream>
+using namespace std;
+
+void Circle::Draw()
+{
+	cout<<"Circle::Draw()..."<<endl;
+}
+Circle::~Circle()
+{
+	cout<<"~Circle..."<<endl;
+}
+
+void Square::Draw()
+{
+	cout<<"Square::Draw()..."<<endl;
+}
+Square::~Square()
+{
+	cout<<"~Square..."<<endl;
+}
+
+void Rectangle::Draw()
+{
+	cout<<"Rectangle::Draw()..."<<endl;
+}
+Rectangle::~Rectangle()
+{
+	cout<<"~Rectangle..."<<endl;
+}
+
---
+++ src/linux-c/36cpp/Shape.h
@@ -0,0 +1,31 @@
+#ifndef _SHAPE_H_
+#define _SHAPE_H_
+
+class Shape
+{
+public:
+        virtual void Draw() = 0;
+        virtual ~Shape() {}
+};
+
+class Circle : public Shape
+{
+public:
+        void Draw();
+        ~Circle();
+};
+
+class Square : public Shape
+{
+public:
+        void Draw();
+        ~Square();
+};
+
+class Rectangle : public Shape
+{
+        void Draw();
+	 ~Rectangle();
+};
+
+#endif //_SHAPE_H_
